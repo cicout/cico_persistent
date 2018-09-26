88840378c1d323362f2fb7978c43dce7745015bb
lucky.li <wxindfx@gmail.com>
2018-09-26 11:14:29 +0800

    update for swift 4.2;

--- CICOPersistentDemo/ViewController.swift
+++ CICOPersistentDemo/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("\(CICOPathAide.docPath(withSubPath: nil))")
+        print("\(CICOPathAide.docPath(withSubPath: nil)!)")
     }
 
     override func didReceiveMemoryWarning() {

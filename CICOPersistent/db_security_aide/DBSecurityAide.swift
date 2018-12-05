401c5abc97b463cb02834d3cbbb7e62fa7aa9108
lucky.li <wxindfx@gmail.com>
2018-12-05 20:18:01 +0800

    update note;

--- CICOPersistent/db_security_aide/DBSecurityAide.swift
+++ CICOPersistent/db_security_aide/DBSecurityAide.swift
@@ -9,6 +9,16 @@
 import Foundation
 
 public class DBSecurityAide {
+    /// Export database;
+    ///
+    /// - parameter fromDBPath: Source database path;
+    /// - parameter fromDBPassword: Source database password, no password when nil;
+    ///             The password will be transfered to md5 hash for KVDBService/ORMDBService using;
+    /// - parameter toDBPath: Destination database path;
+    /// - parameter toDBPassword: Destination database password, no password when nil;
+    ///             The password will be transfered to md5 hash for KVDBService/ORMDBService using;
+    ///
+    /// - returns: Export result;
     static public func exportDatabase(fromDBPath: String,
                                       fromDBPassword: String?,
                                       toDBPath: String,
@@ -31,18 +41,41 @@ public class DBSecurityAide {
         return result
     }
     
+    /// Encrypt passwordless database;
+    ///
+    /// - parameter dbPath: Passwordless database path;
+    /// - parameter password: Database encryption password;
+    ///             The password will be transfered to md5 hash for KVDBService/ORMDBService using;
+    ///
+    /// - returns: Encrypt result;
     static public func encryptDatabase(dbPath: String, password: String) -> Bool {
         let realPassword = CICOSecurityAide.md5HashString(with: password)
         let result = CICOSQLCipherAide.encryptDatabase(dbPath, password: realPassword)
         return result
     }
-    
+
+    /// Decrypt encrypted database into passwordless;
+    ///
+    /// - parameter dbPath: Encrypted database path;
+    /// - parameter password: Encrypted database password;
+    ///             The password will be transfered to md5 hash for KVDBService/ORMDBService using;
+    ///
+    /// - returns: Decrypt result;
     static public func decryptDatabase(dbPath: String, password: String) -> Bool {
         let realPassword = CICOSecurityAide.md5HashString(with: password)
         let result = CICOSQLCipherAide.decryptDatabase(dbPath, password: realPassword)
         return result
     }
     
+    /// Change encrypted database password;
+    ///
+    /// - parameter dbPath: Encrypted database path;
+    /// - parameter originalPassword: Original password for encrypted database;
+    ///             The password will be transfered to md5 hash for KVDBService/ORMDBService using;
+    /// - parameter newPassword: New password for encrypted database;
+    ///             The password will be transfered to md5 hash for KVDBService/ORMDBService using;
+    ///
+    /// - returns: Change password result;
     static public func changeDatabasePassword(dbPath: String, originalPassword: String, newPassword: String) -> Bool {
         let realOriginalPassword = CICOSecurityAide.md5HashString(with: originalPassword)
         let realNewPassword = CICOSecurityAide.md5HashString(with: newPassword)
--- CICOPersistent/tool/sql_cipher_aide/CICOSQLCipherAide.h
+++ CICOPersistent/tool/sql_cipher_aide/CICOSQLCipherAide.h
@@ -12,15 +12,50 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CICOSQLCipherAide : NSObject
 
+/**
+ * Export database;
+ *
+ * @param fromDBPath Source database path;
+ * @param fromDBPassword Source database password, no password when nil;
+ * @param toDBPath Destination database path;
+ * @param toDBPassword Destination database password, no password when nil;
+ *
+ * @return Export result;
+ */
 + (BOOL)exportDatabase:(NSString *)fromDBPath
         fromDBPassword:(nullable NSString *)fromDBPassword
               toDBPath:(NSString *)toDBPath
           toDBPassword:(nullable NSString *)toDBPassword;
 
+/**
+ * Encrypt passwordless database;
+ *
+ * @param dbPath Passwordless database path;
+ * @param password Database encryption password;
+ *
+ * @return Encrypt result;
+ */
 + (BOOL)encryptDatabase:(NSString *)dbPath password:(NSString *)password;
 
+/**
+ * Decrypt encrypted database into passwordless;
+ *
+ * @param dbPath Encrypted database path;
+ * @param password Encrypted database password;
+ *
+ * @return Decrypt result;
+ */
 + (BOOL)decryptDatabase:(NSString *)dbPath password:(NSString *)password;
 
+/**
+ * Change encrypted database password;
+ *
+ * @param dbPath Encrypted database path;
+ * @param originalPassword Original password for encrypted database;
+ * @param newPassword New password for encrypted database;
+ *
+ * @return Change password result;
+ */
 + (BOOL)changeDatabasePassword:(NSString *)dbPath
               originalPassword:(NSString *)originalPassword
                    newPassword:(NSString *)newPassword;

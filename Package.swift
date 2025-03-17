7059b3a7bd10dffd0fb7c435b9dcee3f83d091ca
Ethan.Li <wxindfx@gmail.com>
2025-03-17 11:47:25 +0800

    update spm;

--- Package.swift
+++ Package.swift
@@ -1,22 +1,30 @@
-// swift-tools-version:5.1
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "CICOPersistent",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "CICOPersistent", targets: ["CICOPersistent"])
     ],
     dependencies: [
+        .package(url: "https://github.com/cicout/cico_foundation_kit.git",
+                 from: "0.2.13"),
         .package(url: "https://github.com/cicout/cico_auto_codable.git",
-                 from: "0.20.24"),
-        .package(url: "https://github.com/cicout/fmdb_with_sqlcipher_p2c.git",
-                 PackageDescription.Package.Dependency.Requirement._branchItem("master"))
+                 from: "0.30.44"),
+        .package(url: "https://github.com/ccgus/fmdb.git",
+                 from: "2.7.12")
     ],
     targets: [
         .target(
             name: "CICOPersistent",
+            dependencies: [
+                .product(name: "CICOFoundationKit", package: "cico_foundation_kit"),
+                .product(name: "CICOAutoCodable", package: "cico_auto_codable"),
+                .product(name: "FMDB", package: "fmdb")
+            ],
             path: "CICOPersistent"
         )
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
